612615266590da1cdc8c91fc0801bd20cf1ff884
Fikri Rasyid <fikrirasyid@gmail.com>
2014-12-12 15:59:24 +0700

    Adding transient, adding method for getting sale product ids

--- inc/wc-sale-overview-product.php
+++ inc/wc-sale-overview-product.php
@@ -12,31 +12,80 @@ class WC_Sale_Overview_Product{
 	public function get_all_variable_products_ids(){
 		global $wpdb;
 
-		$posts = $wpdb->get_results("SELECT posts.ID FROM 
-								{$wpdb->posts} posts, 
-								{$wpdb->term_relationships} term_relationships, 
-								{$wpdb->term_taxonomy} term_taxonomy,
-								{$wpdb->terms} terms
-								WHERE terms.name = 'variable'
-								AND terms.term_id = term_taxonomy.term_id
-								AND term_taxonomy.term_taxonomy_id = term_relationships.term_taxonomy_id
-								AND term_relationships.object_id = posts.ID
-								AND posts.post_type = 'product'
-								AND posts.post_status = 'publish'");
+		$transient_key = 'wc_sale_overview_variable_products_ids';
 
-		$posts_ids = array();
+		$posts_ids = get_transient( $transient_key );
 
-		// Prepare the result
-		if( ! empty( $posts ) ){
+		if( ! $posts_ids ){
+			$posts = $wpdb->get_results("SELECT posts.ID FROM 
+									{$wpdb->posts} posts, 
+									{$wpdb->term_relationships} term_relationships, 
+									{$wpdb->term_taxonomy} term_taxonomy,
+									{$wpdb->terms} terms
+									WHERE terms.name = 'variable'
+									AND terms.term_id = term_taxonomy.term_id
+									AND term_taxonomy.term_taxonomy_id = term_relationships.term_taxonomy_id
+									AND term_relationships.object_id = posts.ID
+									AND posts.post_type = 'product'
+									AND posts.post_status = 'publish'");
 
-			foreach ( $posts as $post ) {
+			$posts_ids = array();
 
-				$posts_ids[] = intval( $post->ID );
+			// Prepare the result
+			if( ! empty( $posts ) ){
 
+				foreach ( $posts as $post ) {
+
+					$posts_ids[] = intval( $post->ID );
+
+				}
 			}
+
+			set_transient( $transient_key, $posts_ids, YEAR_IN_SECONDS );
 		}
 
 		return $posts_ids;
+	}
+
+	/**
+	 * Get sale product ID
+	 * Apparently wc_get_product_ids_on_sale() isn't as reliable as it should be.
+	 * Sometimes when user deletes product, the transient weren't deleted
+	 * Which causes a quite of a problem 
+	 * 
+	 * @return array
+	 */
+	public function get_sale_products_ids(){
+
+		$transient_key = 'wc_sale_overview_sale_products_ids';
+
+		$products_ids = get_transient( $transient_key );
+
+		if( ! $products_ids ){
+
+			$sale_products_ids = wc_get_product_ids_on_sale();
+
+			$products_args = array(
+				'post_type' 		=> array( 'product', 'product_variation' ),
+				'post__in' 			=> $sale_products_ids,
+				'posts_per_page'	=> -1
+			);
+
+			$products_obj = get_posts( $products_args );
+
+			$products_ids = array();
+
+			foreach ( $products_obj as $product ) {
+				$products_ids[] = $product->ID;
+			}
+
+			$expiration = 60 * 60 * 24; // A day
+
+			set_transient( $transient_key, $products_ids, $expiration );
+
+		}
+
+		return $products_ids;
 	}
 
 	/**
@@ -45,28 +94,41 @@ class WC_Sale_Overview_Product{
 	 * @return array
 	 */
 	public function get_scheduled_products_ids(){
-		$current_time = current_time( 'timestamp' );
 
-		$args = array(
-			'post_type' 	=> array( 'product', 'product_variation' ),
-			'post_status' 	=> 'publish',
-			'posts_per_page'=> -1,
-			'meta_key'		=> '_sale_price_dates_from',
-			'meta_value'	=> $current_time,
-			'meta_compare'	=> '>='
-		);
+		$transient_key = 'wc_sale_overview_scheduled_products_ids';
 
-		$products = get_posts( $args );
+		$posts_ids = get_transient( $transient_key );
 
-		$ids = array();
+		if( ! $posts_ids ){
 
-		if( ! empty( $products ) ){
-			foreach ( $products as $product ) {
-				$ids[] = $product->ID;
+			$current_time = current_time( 'timestamp' );
+
+			$args = array(
+				'post_type' 	=> array( 'product', 'product_variation' ),
+				'post_status' 	=> 'publish',
+				'posts_per_page'=> -1,
+				'meta_key'		=> '_sale_price_dates_from',
+				'meta_value'	=> $current_time,
+				'meta_compare'	=> '>='
+			);
+
+			$products = get_posts( $args );
+
+			$posts_ids = array();
+
+			if( ! empty( $products ) ){
+				foreach ( $products as $product ) {
+					$posts_ids[] = $product->ID;
+				}
 			}
+
+			$expiration = 60 * 60 * 24; // A day
+
+			set_transient( $transient_key, $posts_ids, $expiration );
+
 		}
 
-		return array_unique( $ids );
+		return $posts_ids;
 	}
 
 	/**
